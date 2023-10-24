0b3071cc7fe009d72ef9f276d57ca1986a89a6a0
Asad Ali <asadzali89@hotmail.com>
2023-10-23 23:40:57 -0500

    About Us update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,9 +8,9 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify", color: "black"}}>
-            Howdy, my name is <span className="purple">Asad Ali </span>
+            Howdy, my name is <span className="purple">Arsalan Ali </span>
             and I am from <span className="purple"> the great state of Texas.</span>
-            <br />I am a Full time Software Developer at Pariveda Solutions in Houston Tx.
+            <br />I am a Full time Student at Texas A&M University.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -20,17 +20,17 @@ function AboutCard() {
               <ImPointRight /> Playing Video Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Designing Cars
+              <ImPointRight /> 3d Printing
             </li>
             <li className="about-activity">
-              <ImPointRight /> Ultimate Frisbee
+              <ImPointRight /> Basketball
             </li>
           </ul>
 
           <p style={{ color: "#5EBEC4" }}>
-            "Never Cut What You can Untie"{" "}
+            "This is my last loss"{" "}
           </p>
-          <footer className="blockquote-footer">Robert Frost</footer>
+          <footer className="blockquote-footer">Michael "K3Soju" Zhang</footer>
         </blockquote>
       </Card.Body>
     </Card>
--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="asadzali89"
+        username="arsalanzali"
         blockSize={15}
         blockMargin={5}
         color="grey"
